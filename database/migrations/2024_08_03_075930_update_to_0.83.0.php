c89284a726aac90da9f90ed883a4534f9b76695a
bahreynipour <bahreynipour@gmail.com>
2024-08-03 11:31:59 +0330

    add migration for core updates

---
+++ database/migrations/2024_08_03_075930_update_to_0.83.0.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared(file_get_contents(__DIR__ . '/sql/0.80.0-0.83.0.sql'));
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
